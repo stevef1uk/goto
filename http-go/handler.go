01c40267ee402b99be192b5d8bad853641e4c0f6
stevef1uk <stevef1uk@users.noreply.github.com>
2021-02-06 16:34:38 +0100

    Update handler.go

--- http-go/handler.go
+++ http-go/handler.go
@@ -11,7 +11,7 @@ import (
 func Handle(req handler.Request) (handler.Response, error) {
 	var err error
 
-	message := fmt.Sprintf("Hello world Steven & Sarah plus Lilith & Freya cats in France, input was: %s", string(req.Body))
+	message := fmt.Sprintf("Hello world Steven & Sarah plus Lilith & Freya cats in France , input was: %s", string(req.Body))
 
 	return handler.Response{
 		Body:       []byte(message),
